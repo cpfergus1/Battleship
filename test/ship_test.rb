e1aaf2e836481bd68350f78385d13cb294350637
Connor Ferguson <cpfergus1@gmail.com>
2020-08-25 08:19:52 -0600

    Initial attribute ship testing logic added

---
+++ test/ship_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship.rb'
+
+
+class TestShip <Minitest::Test
+  def setup
+    @cruiser = Ship("Cruiser", 3)
+  end
+
+def test_instance_of
+  assert_instance_of Ship, @cruiser
+end
+
+def test_ship_has_attributes
+  assert_equal "Cruiser", @cruiser.name
+  assert_equal 3, @cruiser.length
+  assert_equal 3, @cruiser.health
+end
