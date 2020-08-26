8638913faf851ae65930fea7fd88edded6413f10
Connor Ferguson <cpfergus1@gmail.com>
2020-08-25 21:56:57 -0600

    Implemented test for cells in board

--- test/board_test.rb
+++ test/board_test.rb
@@ -10,4 +10,13 @@ class BoardTest <Minitest::Test
     board = Board.new
     assert_instance_of Board, board
   end
+
+  def test_board_has_attributes
+    board = Board.new
+    assert_nil nil, board.cells
+    assert_equal 16, board.cells.keys.length
+    assert_equal 16, board.cells.values.length
+    assert_equal "A1", board.cells.values[0].coordinate
+    assert_equal "D4", board.cells.values[15].coordinate
+  end
 end
