0e8114c493d1fdac9c2c24f7af9535d1b498134d
John Kim <abcdefghijohnk@gmail.com>
2020-08-25 20:09:12 -0600

    create place ship test, passes

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,8 +1,19 @@
 class Cell
-  attr_reader :coordinate, :ship 
+  attr_reader :coordinate, :ship
+
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @empty = true
 
+  end
+
+  def empty?
+    @empty
+  end
+
+  def place_ship(ship)
+    @ship = ship
+    @empty = false 
   end
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -9,6 +9,21 @@ class  CellTest<  Minitest::Test
 
     assert_instance_of Cell, cell
     assert_equal "B4", cell.coordinate
-    assert_nil nil, cell.ship 
+    assert_nil nil, cell.ship
+  end
+
+  def test_cell_starts_empty
+    cell = Cell.new("B4")
+
+    assert_equal true, cell.empty?
+  end
+
+  def test_it_can_place_ship
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+
+    assert_equal cruiser, cell.ship
+    assert_equal false, cell.empty?
   end
 end
