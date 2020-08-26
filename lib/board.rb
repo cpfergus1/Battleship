07b6b236bd35fe04dbbd69f04efd90d5a7c6826a
Connor Ferguson <cpfergus1@gmail.com>
2020-08-25 21:57:50 -0600

    Implemented cells method

--- lib/board.rb
+++ lib/board.rb
@@ -3,5 +3,19 @@ require './lib/cell.rb'
 
 class Board
 
+  def cells
+    cells = {}
+    board_length = [1,2,3,4]
+    board_height = ['A','B','C','D']
+    board_height.each do |letter|
+      board_length.each do |number|
+        cell = Cell.new("#{letter}#{number}")
+        cells["#{letter}#{number}"] = cell
+      end
+    end
+    cells
+    #require 'pry' ; binding.pry
+  end
+
 
 end
